7bc293473a0de4217f7668fc4998bce47ec04b16
 <96326525+mutasim77@users.noreply.github.com>
2024-06-18 03:39:35 +0500

    💳 fix: remove dockerfile && mv error file

--- src/models/UserModels.ts
+++
@@ -1,9 +0,0 @@
-import { serial, text, timestamp, pgTable } from 'drizzle-orm/pg-core';
-
-export const users = pgTable('users', {
-    id: serial('id').primaryKey(),
-    name: text('name').notNull(),
-    email: text('email').unique().notNull(),
-    password: text('password').notNull(),
-    created_at: timestamp('created_at').defaultNow().notNull(),
-});
