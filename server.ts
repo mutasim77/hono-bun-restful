69857ab4a531cc14a3ec62dfb1f9feab67a45722
 <96326525+mutasim77@users.noreply.github.com>
2024-06-18 03:33:33 +0500

    🐣 fix: import pathes

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
-import { errorHandler, notFound } from './src/middlewares/error'
+import { errorHandler, notFound } from './src/utils/error'
+import { userRoutes } from './src/routes';
 
 //? Init the Hone App;
 const app = new Hono().basePath('/api/v1');
@@ -8,10 +9,10 @@ const app = new Hono().basePath('/api/v1');
 app.get('/', (c) => c.text('Welcome to the API'));
 
 //? User Routes
-app.route('/users');
+app.route('/users', userRoutes);
 
 //? Error Handler
-app.onError((err, c) => {
+app.onError((_, c) => {
     const error = errorHandler(c);
     return error;
 });
